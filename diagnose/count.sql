7f47278364e2aa64907e6328eb6a1bf1bf060078
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-04-28 11:54:44 -0500

    preparando repositorio sap_netezza

---
+++ diagnose/count.sql
@@ -0,0 +1,12 @@
+SELECT BUDAT, COUNT(*) AS count FROM CRUDE_BSEG_5200
+GROUP BY BUDAT
+ORDER BY BUDAT
+-----------------------------------------------------------
+SELECT BUDAT, COUNT(*) AS count FROM CRUDE_BSEG_5300
+GROUP BY BUDAT
+ORDER BY BUDAT
+-----------------------------------------------------------
+SELECT BUDAT, COUNT(*) AS count FROM CRUDE_BSEG_5500
+GROUP BY BUDAT
+ORDER BY BUDAT
+-----------------------------------------------------------
